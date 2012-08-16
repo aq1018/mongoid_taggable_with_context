b417112da996f2cb0b6647dd551eea3f0e010148
Rocky Duan <dementrock@gmail.com>
2012-08-15 17:59:14 -0700

    passed all tests

--- lib/mongoid/taggable_with_context/aggregation_strategy/real_time.rb
+++ lib/mongoid/taggable_with_context/aggregation_strategy/real_time.rb
@@ -11,7 +11,7 @@ module Mongoid::TaggableWithContext::AggregationStrategy
       # Collection name for storing results of tag count aggregation
 
       def aggregation_database_collection_for(context)
-        @aggregation_database_collection ||= Moped::Collection.new(self.collection.database, aggregation_collection_for(context))
+        (@aggregation_database_collection ||= {})[context] ||= Moped::Collection.new(self.collection.database, aggregation_collection_for(context))
       end
 
       def aggregation_collection_for(context)
@@ -25,7 +25,7 @@ module Mongoid::TaggableWithContext::AggregationStrategy
       # retrieve the list of tag with weight(count), this is useful for
       # creating tag clouds
       def tags_with_weight_for(context, conditions={})
-        aggregation_database_collection_for(context).find({:value => {"$gt" => 0 }}).sort(_id: 1).to_a.map{ |t| [t["_id"], t["value"]] }
+        aggregation_database_collection_for(context).find({:value => {"$gt" => 0 }}).sort(_id: 1).to_a.map{ |t| [t["_id"], t["value"].to_i] }
       end
 
       def recalculate_all_context_tag_weights!
